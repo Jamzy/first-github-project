3c72a00758fc2c7f7f57d9ab0d553928045533c5
Jamzy <j.obrien4412@gmail.com>
2022-07-06 13:27:49 +0100

    Film Search API Complete

--- Week 4/Final Project/index.js
+++ Week 4/Final Project/index.js
@@ -2,13 +2,18 @@ const filmListEl = document.querySelector('.films--list')
 const spinnerWrapper = document.querySelector('.spinner__wrapper')
 
 async function main(event) {
+    spinnerWrapper.classList += ' films--loading'
     
-    spinnerWrapper.classList += ' films--list'
+    setTimeout(() => {
+        
+    }, 2000);
     const search = event.target.value
     const films = await fetch (`http://www.omdbapi.com/?i=tt3896198&apikey=411ac4a7&s=${search}`)
+
+
     const filmsData = await films.json()
     filmListEl.innerHTML = filmsData.Search.slice(0, 9).map(film => filmHTML(film)).join('')
-    spinnerWrapper.classList.remove('films--list')
+    spinnerWrapper.classList.remove('films--loading')
 
 }
 
@@ -19,6 +24,5 @@ function filmHTML(film) {
     <img src="${film.Poster}" alt="" class="film--img">
     <h3 class="film--title">${film.Title}</h3>
     <h4 class="film--year"><i class="fa-solid fa-calendar-days"></i>${film.Year}</h4>
-    <h4 class="film--rating">${film.Ratings[0].Value}</h4>
 </div>`
   }
