96dfd459c519cfcdc772cbae5bb4db9b8e6dad43
Jamzy <j.obrien4412@gmail.com>
2022-07-10 01:57:49 +0100

    Class and State Practice

---
+++ Week 5/class-and-states/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import App from './FCC Practice/app'
+
+
+function AppS() {
+  return (
+  <App />
+  )
+}
+
+export default App;
--- Week 5/week-5/src/components/MyApp.js
+++ Week 5/week-5/src/components/MyApp.js
@@ -3,17 +3,6 @@ import Joke from "./jokes";
 import './style.css';
 
 
-function MyApp() {
-   
 
-  return (
-    
-    <div className="todo--list">
-        <Joke question="" punchLine="A spelling bee."/>
-        <Joke question="What’s more amazing than a talking dog?" punchLine="A spelling bee."/>
-        
-    </div>
-  )
-}
 
 export default MyApp;
